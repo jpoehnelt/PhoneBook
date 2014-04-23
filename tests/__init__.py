fbeeb71f41f414ba9772b2f363c80836ebbc3935
justinwp <justin.poehnelt@gmail.com>
2014-04-23 14:06:38 -0700

    add a test for hash

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,9 @@
-__author__ = 'Justin'
+from unittest import TestCase
+from phone_book import Listing
+
+
+class TestListing(TestCase):
+    def test_hash(self):
+        hash_length = 100
+        p1 = Listing(9287749178, 'Test', hash_length)
+        self.assertEqual(hash(p1),93)
