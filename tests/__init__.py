2937e88e6e69e1b6c733ceae6dba774b0a0c1595
justinwp <justin.poehnelt@gmail.com>
2014-04-24 14:55:59 -0700

    run tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,12 @@
-from unittest import TestCase
+import unittest
 from phone_book import Listing
 
 
-class TestListing(TestCase):
+class TestListing(unittest.TestCase):
     def test_hash(self):
         hash_length = 100
         p1 = Listing(9287749178, 'Test', hash_length)
         self.assertEqual(hash(p1),93)
+
+if __name__ == '__main__':
+    unittest.main()
