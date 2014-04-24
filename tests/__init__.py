86c2da42ba27be48c0b1e609a029e03297dab5d4
justinwp <justin.poehnelt@gmail.com>
2014-04-24 16:51:03 -0700

    add settings test for hash table length

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,42 @@
 import unittest
-from phone_book import Listing
+from phone_book import Listing, Book
 
 
 class TestListing(unittest.TestCase):
     def test_hash(self):
         hash_length = 100
         p1 = Listing(9287749178, 'Test', hash_length)
-        self.assertEqual(hash(p1),93)
+        self.assertEqual(hash(p1), 93)
+
+
+class TestBook(unittest.TestCase):
+    def setUp(self):
+        self.bk = Book()
+
+    def tearDown(self):
+        pass
+
+    def test_add(self):
+        self.bk.add(9287749178, 'Flagstaff Soap Company LLC')
+        self.assertEqual(len(self.bk), 1)
+        self.bk.add(9287749170, 'Test')
+        self.bk.add(9287749171, 'Another Test')
+        self.assertEqual(len(self.bk), 3)
+
+    def test_sorts_on_add(self):
+        self.bk.add(9287749178, 'Flagstaff Soap Company LLC')
+        self.bk.add(9287749170, 'Test')
+        self.bk.add(9287749171, 'Another Test')
+        self.assertEqual(self.bk[0]['num'], 9287749171)
+        self.assertEqual(self.bk[1]['num'], 9287749178)
+        self.assertEqual(self.bk[2]['num'], 9287749170)
+
+    def test_settings(self):
+        self.bk.configure(HASH_TABLE_LENGTH=10)
+        self.assertEqual(self.bk.settings()['HASH_TABLE_LENGTH'], 10)
+        self.bk.add(9287749170, 'Test')
+        self.assertEqual(len(self.bk.hash_table('num')), 10)
+
 
 if __name__ == '__main__':
     unittest.main()
