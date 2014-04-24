433d240d39d8864d2df069e230f39a540a08e0bb
justinwp <justin.poehnelt@gmail.com>
2014-04-24 12:41:15 -0700

    modified install

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-__author__ = 'Justin'
+from setuptools import setup
+
+setup(
+    author='Justin Poehnelt',
+    author_email='justin.poehnelt@gmail.com',
+    description='A phone book implementation for an assignment',
+    include_package_data=True,
+    name='PhoneBookJP',
+    platforms='any',
+    packages=['phone_book'],
+    version='0.1.0-dev',
+    zip_safe=False
+)
