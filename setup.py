afbda296083c09669a278f13f8a0678410e3f265
justinwp <justin.poehnelt@gmail.com>
2014-04-24 17:37:42 -0700

    name change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author_email='justin.poehnelt@gmail.com',
     description='A phone book implementation for an assignment',
     include_package_data=True,
-    name='PhoneBookJP',
+    name='PhoneBook',
     platforms='any',
     packages=['phone_book'],
     version='0.1.0-dev',
